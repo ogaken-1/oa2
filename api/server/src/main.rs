7574bd265079da8c02c6aeb876581470e7993fe6
Kento Ogata <k.ogata1013@gmail.com>
2024-05-05 21:39:40 +0900

    use result type

--- api/server/src/main.rs
+++ api/server/src/main.rs
@@ -1,8 +1,16 @@
-use axum::{routing::get, Router};
+async fn run() -> std::io::Result<()> {
+    use axum::{routing::get, Router};
+
+    let router = Router::new();
+    let app = router.route("/", get(|| async { "Hello world!" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    axum::serve(listener, app).await?;
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello world!" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    if let Err(err) = run().await {
+        eprintln!("Application downed: {}", err);
+    }
 }
