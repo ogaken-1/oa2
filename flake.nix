933279f4afd28328eeabe39e049d735f7a7ee088
Kento Ogata <k.ogata1013@gmail.com>
2024-05-14 23:11:16 +0900

    flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) optional;
+        pkgs = import nixpkgs { inherit system; };
+
+        nodejs = pkgs.nodejs;
+        corepack = pkgs.stdenv.mkDerivation {
+          name = "corepack";
+          buildInputs = [ nodejs ];
+          phases = [ "installPhase" ];
+          installPhase = ''
+            mkdir -p $out/bin
+            corepack enable --install-directory=$out/bin
+          '';
+        };
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            nodejs
+            corepack
+          ];
+        };
+      }
+    );
+}
