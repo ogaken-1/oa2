a610bbbacc87fa5ebf877fdb567b85a0913780a6
Kento Ogata <k.ogata1013@gmail.com>
2024-06-10 20:06:38 +0900

    perf: More lazy

--- web-server/src/index.tsx
+++ web-server/src/index.tsx
@@ -2,11 +2,11 @@ import { Suspense, render } from "solid-js/web";
 import "./index.css";
 import { Route, Router } from "@solidjs/router";
 import NavBar from "./components/navbar";
-import NotFound from "./not-found";
 import { lazy } from "solid-js";
 
 const Home = lazy(() => import("./routes/index"));
 const About = lazy(() => import("./routes/about"));
+const NotFound = lazy(() => import("./not-found"));
 
 const root = document.getElementById("root");
 if (root != null) {
